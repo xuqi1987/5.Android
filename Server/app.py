114e17aaff80b95308e7dd81aa18d29dfb2ee6a0
xuqi1987 <xuqi.jx@gmail.com>
2016-03-15 10:59:43 +0800

    add route /sound

--- Server/app.py
+++ Server/app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask,jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/sound')
+def sound():
+
+    return jsonify(id=1)
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=80)
+    app.run(host="0.0.0.0",port=8080)
