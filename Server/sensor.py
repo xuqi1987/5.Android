3e068adc0e8748fa4cf3b3115d4b84a1e78e88f0
xuqi1987 <xuqi.jx@gmail.com>
2016-03-19 19:08:33 +0800

    update

--- Server/sensor.py
+++ Server/sensor.py
@@ -25,7 +25,7 @@ class Sensor:
 
         # 减少误差
         avg = 0
-        for i in  range(4):
+        for i in  range(5):
 
             if (GPIO.input(14) == GPIO.HIGH):
 
@@ -34,7 +34,7 @@ class Sensor:
             else:
                 avg = avg - 1
                 print "OFF"
-            time.sleep(0.01)
+
 
         if avg > 0:
             self.value = self.value + 1
