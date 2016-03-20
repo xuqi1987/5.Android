b2d677c6a311c88d29cc71475ded9eea7ef9d9ca
xuqi1987 <xuqi.jx@gmail.com>
2016-03-20 16:08:25 +0800

    add readme

--- Server/sensor.py
+++ Server/sensor.py
@@ -10,6 +10,7 @@ class Sensor:
         self.count = 0
         pass
 
+    # 初始化树莓派接口14
     def setup(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(14,GPIO.IN)
@@ -18,12 +19,13 @@ class Sensor:
     def destory(self):
         GPIO.cleanup()
         pass
-
+    
+    # 取的当前状态
     def get(self):
         self.count = self.count + 1
         ret = False
 
-        # 减少误差
+        # 减少误差,读取2000次
         max_num = 2000
         l = []
         for i in range(max_num):
@@ -33,12 +35,12 @@ class Sensor:
                 l.append('ON')
 
         print ("%s / %s ")%(len(l),max_num)
-
+        # 如果有800次是ON的话,认为是在哭
         if len(l)> (800):
             self.cry = True
             print "Baby maybe crying~"
 
-
+        # 每5次清空一次
         if self.count > 5:
             print "Clear"
             self.count = 0
